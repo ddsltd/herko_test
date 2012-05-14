4e444439d5a833f2232d5601bef4541cd205fd6f
Mahesh Dsouza <mahesh.dsouza@gmail.com>
2012-05-13 23:19:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerkoTest::Application.config.secret_token = 'f4d584a3b489070d636dbad00e108b713e89ea3fe8865431b1b8c647f35e4c6f9aea839c676b976c2430670b96ca87be513ef51b59a5b04741dbfa97171f750c'
